5d58076fd72c5ad2025229f1bf16be179923fd83
victorimannuel <victorimannuel.dev@gmail.com>
2024-12-13 12:47:29 +0700

    build: add module ias_sale
    - show delivery date in tree and front form view
    - adjust menu sequence

---
+++ addons/ias_sale/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'license': 'LGPL-3',
+    'name': "Ivy and Sage Sale",
+    'summary': "Ivy and Sage Sale",
+    'author': "Victor Imannuel (Child of God)",
+    'website': "",
+    'support': 'victorimannuel.dev@gmail.com',
+    'category': 'Sale',
+    'version': '18.0',
+    'depends': [
+        'sale',
+    ],
+    'demo': [
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+        'views/menu_views.xml',
+    ],
+    'images': [
+    ],
+    'assets': {
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
