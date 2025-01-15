2f048b8724119fc204165075c725be36ef5b27d7
victorimannuel <victorimannuel.dev@gmail.com>
2025-01-15 07:08:48 +0700

    feat: modify product image processing from url

--- addons/vt_odoo_faire/models/product_template.py
+++ addons/vt_odoo_faire/models/product_template.py
@@ -10,14 +10,24 @@ class ProductTemplate(models.Model):
     faire_variant_id = fields.Char(string='Faire Variant ID')
     faire_product_image_url = fields.Char(string='Faire Product Image URL')
     
-    @api.onchange('faire_product_image_url')
-    def _onchange_faire_product_image_url(self):
-        if self.faire_product_image_url:
+    def _process_faire_product_image_url(self, url):
+        if url:
             try:
-                response = requests.get(self.faire_product_image_url, timeout=10)
+                response = requests.get(url, timeout=20)
                 if response.status_code == 200:
                     self.image_1920 = base64.b64encode(response.content)
             except Exception:
                 self.image_1920 = False
-        else:
-            self.image_1920 = False
+            
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            print(vals)
+            if 'faire_product_image_url' in vals:
+                self._process_faire_product_image_url(vals['faire_product_image_url'])
+        return super().create(vals_list)
+    
+    def write(self, vals):
+        if 'faire_product_image_url' in vals:
+            self._process_faire_product_image_url(vals['faire_product_image_url'])
+        return super().write(vals)
