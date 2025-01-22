5a8b2f91c850bab3694ab8929c7d74ea97b35dce
victorimannuel <victorimannuel.dev@gmail.com>
2025-01-23 03:08:30 +0700

    chore: update orderflow app manifest

--- addons/orderflow_app/__manifest__.py
+++ addons/orderflow_app/__manifest__.py
@@ -10,12 +10,12 @@
     'depends': [
         # Default odoo module
         'sale_management',
+        'sale',
         'stock',
         'account',
         'utm',
         'mail',
         'contacts',
-        'sale',
         'mass_mailing',
         'mass_mailing_sms',
         'im_livechat',
@@ -24,15 +24,20 @@
         
         # Custom module
         'vt_odoo_faire',
+        'ica_web_responsive',
         'muk_web_theme',
-        'web_chatter_position_cr',
         'web_window_title',
+        'web_chatter_position_cr',
+        'ecommerce_website_minimum_order_quantity',
         
     ],
     'demo': [
     ],
     'data': [
         'views/sale_order_views.xml',
+        'views/product_pricelist_views.xml',
+        'views/delivery_form_templates.xml',
+        
         'data/ir_actions.xml',
         'views/menu_views.xml',
     ],
@@ -41,10 +46,12 @@
     'assets': {
         'web.assets_backend': [
             'orderflow_app/static/src/product_configurator_dialog.js',
-            # 'orderflow_app/static/src/product.xml',
+        ],
+        'web.assets_frontend': [
+            'orderflow_app/static/src/checkout.js',
         ],
     },
     'installable': True,
     'auto_install': False,
-    'application': False,
+    'application': True,
 }
